5efc3d6b7e09cf52b0e775e919e10b0f0231fb96
dirttop <gsyomichev@gmail.com>
2023-07-07 20:30:45 -0400

    little child version

--- Human/human.gd
+++ Human/human.gd
@@ -1,11 +1,30 @@
-extends Node2D
+extends CharacterBody2D
+
+enum {
+	WALK,
+	IDLE,
+	GRABBED,
+	PLANT,
+	BABY,
+	DEAD,
+}
 
 var health;
 var happiness;
 var growth;
+var clickTimer
+
+var State = WALK
+var Speed = randi_range(25, 45)
+var directionX = randf_range(-1,1)
+var directionY = randf_range(-1,1)
+var Change = randf_range(1,4)
+var randIdle = randi_range(0,5)
+var deathTimer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	deathTimer = 300.0
 	health = 5;
 	happiness = 1;
 	growth = 0;
@@ -19,26 +38,75 @@ func _process(delta):
 	#if it is fully grown, it starts losing health slowly (eventually dies)
 	if growth > 500:
 		health -= delta;
+		
+	deathTimer -= delta
+	if deathTimer <= 0.0:
+		State = DEAD
+	if State == WALK:
+		Walk(delta)
+	if State == IDLE:
+		Idle(delta)
+	if State == GRABBED:
+		Grabbed()
 	
-
-
-func _on_button_pressed():
-	var garden = get_parent().get_parent();
-	if !garden.selecting:
-		for n in get_parent().get_children():
-			if n.is_in_group("human"):
-				n.get_node("HumanInfoMenu").visible = false;
+func Plant(delta):
+	pass
+func Baby(delta):
+	pass
+func Dead(delta):
+	pass
+func Walk(delta):
+	velocity.x = directionX*Speed
+	velocity.y = directionY*Speed
+	Change-=delta
+	if Change<=0.0:
+		directionX = randf_range(-1,1)
+		directionY = randf_range(-1,1)
+		Change = randf_range(1,4)
+		randIdle = randi_range(0,3)
+		Speed = randi_range(25, 45)
+		
+	if randIdle == 3:
+		State = IDLE
+	move_and_slide()
+func Idle(delta):
+	velocity.x = 0
+	velocity.y = 0
+	Change-=delta
+	if Change<=0.0:
+		Change = randf_range(1,4)
+		randIdle = randi_range(0,3)
+	
+	if randIdle == 3:
+		State = WALK
+	move_and_slide()
+	
+func Grabbed():
+	self.global_position = get_global_mouse_position()
+	if Input.is_action_just_released("Click"):
 		$HumanInfoMenu.visible = !$HumanInfoMenu.visible;
-	elif garden.selectGoal == "Healthy":
-		health += 5;
-		happiness -= 5;
-		garden.selecting = false;
-	elif garden.selectGoal == "Junk":
-		health -= 5;
-		happiness += 5;
-		garden.selecting = false;
-	elif garden.selectGoal == "Human":
-		health -= 5;
-		happiness -= 5;
-		garden.selecting = false;
-		garden.humanFlesh -= 1;
+		State = WALK
+
+	
+func _on_area_2d_input_event(viewport, event, shape_idx):
+	if Input.is_action_pressed("Click"):
+		State = GRABBED
+		var garden = get_parent().get_parent();
+		if !garden.selecting:
+			for n in get_parent().get_children():
+				if n.is_in_group("human"):
+					n.get_node("HumanInfoMenu").visible = false;
+			$HumanInfoMenu.visible = !$HumanInfoMenu.visible;
+		elif garden.selectGoal == "Healthy":
+			health += 5;
+			happiness -= 5;
+			garden.selecting = false;
+		elif garden.selectGoal == "Junk":
+			health -= 5;
+			happiness += 5;
+			garden.selecting = false;
+		elif garden.selectGoal == "Human":
+			health -= 5;
+			happiness -= 5;
+			garden.selecting = false;
+			garden.humanFlesh -= 1;
